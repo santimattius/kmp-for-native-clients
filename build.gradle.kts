a70c22cad9a45ce7fc83c31d65f7663deb9dac47
Santiago Mattiauda <santimattius@gmail.com>
2025-08-24 14:30:41 -0300

    Revert "[start] native coroutine example"
    This reverts commit 2ac7670adf6546186eec9ceb895d43609efc1640.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
     alias(libs.plugins.androidKMPLibrary) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.nativeCoroutines) apply false
 }
